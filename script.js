0b09d004188a722b19b68b994c057851ade02448
AshutoshAgrawal2004 <agrawalnidhi635@gmail.com>
2019-08-05 18:43:20 +0530

    Added basic index and script files with firebase configured

---
+++ script.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCcTGiaSu-1MVRs6_dX2k0ZAYDSYysOY5Q",
+  authDomain: "my-birthday-666b2.firebaseapp.com",
+  databaseURL: "https://my-birthday-666b2.firebaseio.com",
+  projectId: "my-birthday-666b2",
+  storageBucket: "",
+  messagingSenderId: "892038861110",
+  appId: "1:892038861110:web:300e01b050b0c381"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
